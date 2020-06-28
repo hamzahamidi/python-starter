4f487b225c2b6d28aea655e2a481bc06d54a8ed1
Hamza HAMIDI <22576950+hamzahamidi@users.noreply.github.com>
2020-06-28 23:41:09 +0200

    feat(C++): build & debug vscode

---
+++ program.cpp
@@ -0,0 +1,52 @@
+// vector<int> dp(100000, -1);
+
+// class Solution {
+// public:
+//     int minNumberOfSemesters(int N, vector<vector<int>>& dependencies) {
+//         vector<vector<int>> e(N+1);
+//         vector<int> deg(N+1);  // in deg
+//         for (vector<int> r: dependencies) {
+//             e[r[0]].push_back(r[1]);
+//             deg[r[1]]++;
+//         }
+
+//         queue<int> q;
+//         for (int i = 1; i <= N; ++i) {
+//             if (deg[i] == 0)q.push(i);
+//         }
+
+//         int ans = 0;
+//         int num = 0;
+//         while (q.size()) {
+//             ans++;
+//             num += q.size();
+//             int size = q.size();
+//             while (size--) {
+//                 int x = q.front();
+//                 q.pop();
+//                 for (int y: e[x]) {
+//                     if (--deg[y] == 0) q.push(y);
+//                 }
+//             }
+//         }
+//         if (num != N) return -1;
+//         return ans;
+//     }
+// };
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
